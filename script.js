b97e442d5886496dc38486e71fa52e0582c745c1
ThiongoThiongo <thiongothiongo753@gmail.com>
2024-02-11 05:16:33 -0800

    first

---
+++ script.js
@@ -0,0 +1,20 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+const image = document.querySelector('.sectionPic');
+image.src = '/'
+var imgSrcArray  = ['./assets/grad.JPG', './assets/coffee.jpg', './assets/cfm.jpg', './assets/grads.jpg'], counter = 0
+  
+setInterval(function() {
+ counter++;
+ if(counter ==4)
+ {
+  counter = 0;
+ }
+  image.src = imgSrcArray[counter]
+}, 4000); 
+
