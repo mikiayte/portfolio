f52f5d2346cbfea41f870ac36fb8ee5ac8b1bbc9
ThiongoThiongo <thiongothiongo753@gmail.com>
2024-02-19 04:47:35 -0800

    his

--- script.js
+++ script.js
@@ -7,14 +7,14 @@ function toggleMenu() {
 
 const image = document.querySelector('.sectionPic');
 image.src = '/'
-var imgSrcArray  = ['./assets/grad.JPG', './assets/coffee.jpg', './assets/cfm.jpg', './assets/grads.jpg'], counter = 0
+var imgSrcArray  = ['./assets/grad.JPG', './assets/coffee.jpg', './assets/GENX1B.jpg', './assets/CFMG.jpg', './assets/grads.jpg'], counter = 0
   
 setInterval(function() {
  counter++;
- if(counter ==3)
+ if(counter == 5)
  {
   counter = 0;
  }
   image.src = imgSrcArray[counter]
-}, 1000); 
+}, 2000); 
 
